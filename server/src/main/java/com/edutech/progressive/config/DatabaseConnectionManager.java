cd7430a6e88b3da36d3d71215dbd4bb144cab637
Ramakrishna36 <dummy@gmail.com>
2026-01-17 05:46:31 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
@@ -0,0 +1,13 @@
+package com.edutech.progressive.config;
+
+public class DatabaseConnectionManager {
+
+    public static void loadProperties(){
+
+    }
+
+    public static Connection getConnection(){
+        t
+    }
+
+}
---
+++ server/src/main/java/com/edutech/progressive/entity/Product.java
@@ -0,0 +1,72 @@
+package com.edutech.progressive.entity;
+
+public class Product {
+    private int productid;
+    private int warehouseId;
+    private String productName;
+    private String productDescription;
+    private int quantity;
+    private Long price;
+
+    public Product() {
+    }
+
+    public Product(int productid, int warehouseId, String productName, String productDescription, int quantity,
+            Long price) {
+        this.productid = productid;
+        this.warehouseId = warehouseId;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getProductid() {
+        return productid;
+    }
+
+    public void setProductid(int productid) {
+        this.productid = productid;
+    }
+
+    public int getWarehouseId() {
+        return warehouseId;
+    }
+
+    public void setWarehouseId(int warehouseId) {
+        this.warehouseId = warehouseId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+}
